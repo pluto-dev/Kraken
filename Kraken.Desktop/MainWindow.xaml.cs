20103671bc7ad7e0157798c65c36192ada47edc5
pluto <pluto-dev@mailbox.org>
2024-07-08 18:11:51 +0200

    Add project files

---
+++ Kraken.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using ModernWpf.Controls;
+
+namespace Kraken.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void NavigationView_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+    }
+}
