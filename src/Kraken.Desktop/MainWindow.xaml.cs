24b7a40b2594d948472dec4ea8643cb506d9b724
pluto <pluto-dev@mailbox.org>
2024-08-02 15:35:50 +0200

    Switch from WPF to WinUi

--- src/Kraken.Desktop/MainWindow.xaml.cs
+++ src/Kraken.Desktop/MainWindow.xaml.cs
@@ -1,30 +1,12 @@
-﻿using System.Windows;
-using Kraken.Desktop.Views;
-using ModernWpf.Controls;
+using WinUIEx;
 
 namespace Kraken.Desktop;
 
-public partial class MainWindow
+public sealed partial class MainWindow : WindowEx
 {
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
-    {
-        var navigated = RootFrame.Navigate(typeof(CoolingPage));
-        if (navigated)
-        {
-            var navViewItem = RootNavigationView.MenuItems.OfType<NavigationViewItem>().First();
-            RootNavigationView.SelectedItem = navViewItem;
-        }
-    }
-
-    private void RootNavigationView_OnSelectionChanged(NavigationView sender,
-        NavigationViewSelectionChangedEventArgs args)
-    {
-        if (args.SelectedItemContainer.Tag is not string tag) return;
-        RootFrame.Navigate(Type.GetType(tag));
+        Content = null;
     }
 }
--- src/Kraken.Desktop/Services/KrakenService.cs
+++ src/Kraken.Desktop/Services/KrakenService.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
@@ -5,6 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace Kraken.Desktop.Services;
 
@@ -25,26 +28,18 @@ public class KrakenService
         {
             response = await _http.GetAsync("devices");
         }
-        catch (HttpRequestException e)
-        {
-        }
-        catch (IOException e)
-        {
-        }
-        catch (SocketException e)
-        {
-        }
+        catch (HttpRequestException e) { }
+        catch (IOException e) { }
+        catch (SocketException e) { }
 
-        if (response is not { IsSuccessStatusCode: true }) return null;
+        if (response is not { IsSuccessStatusCode: true })
+            return null;
 
         var content = await response.Content.ReadAsStringAsync();
-        var status = JsonSerializer.Deserialize<List<KrakenDevice>>(
-            content
-        );
+        var status = JsonSerializer.Deserialize<List<KrakenDevice>>(content);
 
         return status;
     }
-
 
     public async Task<KrakenInitStatus?> InitializeKraken(int? id)
     {
@@ -54,25 +49,17 @@ public class KrakenService
         {
             response = await _http.GetAsync($"devices/{id}/initialize");
         }
-        catch (HttpRequestException e)
-        {
-        }
-        catch (IOException e)
-        {
-        }
-        catch (SocketException e)
-        {
-        }
+        catch (HttpRequestException e) { }
+        catch (IOException e) { }
+        catch (SocketException e) { }
 
-        if (response is not { IsSuccessStatusCode: true }) return null;
+        if (response is not { IsSuccessStatusCode: true })
+            return null;
 
         var content = await response.Content.ReadAsStringAsync();
         var status = JsonSerializer.Deserialize<KrakenInitStatus>(
             content,
-            new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            }
+            new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }
         );
 
         return status;
@@ -81,11 +68,14 @@ public class KrakenService
     public async Task SetSpeedProfile(int? id, string channel, List<(int X, int Y)> profile)
     {
         Debug.Assert(id is not null, "Id can't be null");
-        using var json = new StringContent(JsonSerializer.Serialize(new
-        {
-            channel = channel,
-            profile = profile
-        }, options: new() { Converters = { new SpeedProfileConverter() } }), Encoding.UTF8, "application/json");
+        using var json = new StringContent(
+            JsonSerializer.Serialize(
+                new { channel = channel, profile = profile },
+                options: new() { Converters = { new SpeedProfileConverter() } }
+            ),
+            Encoding.UTF8,
+            "application/json"
+        );
 
         using var response = await _http.PostAsync($"devices/{id}/speed", json);
         response.EnsureSuccessStatusCode();
@@ -102,24 +92,16 @@ public class KrakenService
         {
             response = await _http.GetAsync($"devices/{id}/status");
         }
-        catch (HttpRequestException e)
-        {
-        }
-        catch (IOException e)
-        {
-        }
-        catch (SocketException e)
-        {
-        }
+        catch (HttpRequestException e) { }
+        catch (IOException e) { }
+        catch (SocketException e) { }
 
-        if (response is not { IsSuccessStatusCode: true }) return null;
+        if (response is not { IsSuccessStatusCode: true })
+            return null;
         var content = await response.Content.ReadAsStringAsync();
         var status = JsonSerializer.Deserialize<KrakenStatus>(
             content,
-            new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            }
+            new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }
         );
 
         return status;
@@ -132,22 +114,14 @@ public class KrakenService
         {
             response = await _http.GetAsync(DisconnectPath);
         }
-        catch (HttpRequestException e)
-        {
-        }
-        catch (IOException e)
-        {
-        }
-        catch (SocketException e)
-        {
-        }
+        catch (HttpRequestException e) { }
+        catch (IOException e) { }
+        catch (SocketException e) { }
 
         if (response is not null)
         {
             var content = await response.Content.ReadAsStringAsync();
-            var status = JsonSerializer.Deserialize<KrakenStatus>(
-                content
-            );
+            var status = JsonSerializer.Deserialize<KrakenStatus>(content);
 
             return status;
         }
@@ -158,12 +132,20 @@ public class KrakenService
 
 public class SpeedProfileConverter : JsonConverter<ValueTuple<int, int>>
 {
-    public override (int, int) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override (int, int) Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         throw new NotImplementedException();
     }
 
-    public override void Write(Utf8JsonWriter writer, (int, int) value, JsonSerializerOptions options)
+    public override void Write(
+        Utf8JsonWriter writer,
+        (int, int) value,
+        JsonSerializerOptions options
+    )
     {
         writer.WriteStartArray();
         writer.WriteNumberValue(value.Item1);
@@ -174,20 +156,20 @@ public class SpeedProfileConverter : JsonConverter<ValueTuple<int, int>>
 
 public record KrakenInitStatus(
     [property: JsonConverter(typeof(KrakenInitConverter))]
-    (string? Property, string? Value, string? Unit) Firmware,
+        (string? Property, string? Value, string? Unit) Firmware,
     [property: JsonConverter(typeof(KrakenInitConverter))]
-    (string? Property, string Value, string? Unit) Logo,
+        (string? Property, string Value, string? Unit) Logo,
     [property: JsonConverter(typeof(KrakenInitConverter))]
-    (string? Property, string Value, string? Unit) Ring
+        (string? Property, string Value, string? Unit) Ring
 );
 
 public record KrakenStatus(
     [property: JsonConverter(typeof(KrakenStatusConverter))]
-    (string? Property, double Value, string? Unit) Temp,
+        (string? Property, double Value, string? Unit) Temp,
     [property: JsonConverter(typeof(KrakenStatusConverter))]
-    (string? Property, double Value, string? Unit) Speed,
+        (string? Property, double Value, string? Unit) Speed,
     [property: JsonConverter(typeof(KrakenStatusConverter))]
-    (string? Property, double Value, string? Unit) Duty
+        (string? Property, double Value, string? Unit) Duty
 );
 
 public class KrakenStatusConverter : JsonConverter<(string? Property, double Value, string? Unit)>
@@ -249,122 +231,80 @@ public class KrakenInitConverter : JsonConverter<(string? Property, string? Valu
 }
 
 public record ColorChannels(
-    [property: JsonPropertyName("external")]
-    int? External,
+    [property: JsonPropertyName("external")] int? External,
     [property: JsonPropertyName("logo")] int? Logo,
     [property: JsonPropertyName("ring")] int? Ring,
     [property: JsonPropertyName("sync")] int? Sync
 );
 
 public record ColorModes(
-    [property: JsonPropertyName("alternating-3")]
-    IReadOnlyList<int?> Alternating3,
-    [property: JsonPropertyName("alternating-4")]
-    IReadOnlyList<int?> Alternating4,
-    [property: JsonPropertyName("alternating-5")]
-    IReadOnlyList<int?> Alternating5,
-    [property: JsonPropertyName("alternating-6")]
-    IReadOnlyList<int?> Alternating6,
-    [property: JsonPropertyName("backwards-marquee-3")]
-    IReadOnlyList<int?> BackwardsMarquee3,
-    [property: JsonPropertyName("backwards-marquee-4")]
-    IReadOnlyList<int?> BackwardsMarquee4,
-    [property: JsonPropertyName("backwards-marquee-5")]
-    IReadOnlyList<int?> BackwardsMarquee5,
-    [property: JsonPropertyName("backwards-marquee-6")]
-    IReadOnlyList<int?> BackwardsMarquee6,
+    [property: JsonPropertyName("alternating-3")] IReadOnlyList<int?> Alternating3,
+    [property: JsonPropertyName("alternating-4")] IReadOnlyList<int?> Alternating4,
+    [property: JsonPropertyName("alternating-5")] IReadOnlyList<int?> Alternating5,
+    [property: JsonPropertyName("alternating-6")] IReadOnlyList<int?> Alternating6,
+    [property: JsonPropertyName("backwards-marquee-3")] IReadOnlyList<int?> BackwardsMarquee3,
+    [property: JsonPropertyName("backwards-marquee-4")] IReadOnlyList<int?> BackwardsMarquee4,
+    [property: JsonPropertyName("backwards-marquee-5")] IReadOnlyList<int?> BackwardsMarquee5,
+    [property: JsonPropertyName("backwards-marquee-6")] IReadOnlyList<int?> BackwardsMarquee6,
     [property: JsonPropertyName("backwards-moving-alternating-3")]
-    IReadOnlyList<int?> BackwardsMovingAlternating3,
+        IReadOnlyList<int?> BackwardsMovingAlternating3,
     [property: JsonPropertyName("backwards-moving-alternating-4")]
-    IReadOnlyList<int?> BackwardsMovingAlternating4,
+        IReadOnlyList<int?> BackwardsMovingAlternating4,
     [property: JsonPropertyName("backwards-moving-alternating-5")]
-    IReadOnlyList<int?> BackwardsMovingAlternating5,
+        IReadOnlyList<int?> BackwardsMovingAlternating5,
     [property: JsonPropertyName("backwards-moving-alternating-6")]
-    IReadOnlyList<int?> BackwardsMovingAlternating6,
-    [property: JsonPropertyName("backwards-rainbow-flow")]
-    IReadOnlyList<int?> BackwardsRainbowFlow,
+        IReadOnlyList<int?> BackwardsMovingAlternating6,
+    [property: JsonPropertyName("backwards-rainbow-flow")] IReadOnlyList<int?> BackwardsRainbowFlow,
     [property: JsonPropertyName("backwards-rainbow-pulse")]
-    IReadOnlyList<int?> BackwardsRainbowPulse,
+        IReadOnlyList<int?> BackwardsRainbowPulse,
     [property: JsonPropertyName("backwards-spectrum-wave")]
-    IReadOnlyList<int?> BackwardsSpectrumWave,
+        IReadOnlyList<int?> BackwardsSpectrumWave,
     [property: JsonPropertyName("backwards-super-rainbow")]
-    IReadOnlyList<int?> BackwardsSuperRainbow,
-    [property: JsonPropertyName("breathing")]
-    IReadOnlyList<int?> Breathing,
+        IReadOnlyList<int?> BackwardsSuperRainbow,
+    [property: JsonPropertyName("breathing")] IReadOnlyList<int?> Breathing,
     [property: JsonPropertyName("candle")] IReadOnlyList<int?> Candle,
     [property: JsonPropertyName("covering-backwards-marquee")]
-    IReadOnlyList<int?> CoveringBackwardsMarquee,
-    [property: JsonPropertyName("covering-marquee")]
-    IReadOnlyList<int?> CoveringMarquee,
+        IReadOnlyList<int?> CoveringBackwardsMarquee,
+    [property: JsonPropertyName("covering-marquee")] IReadOnlyList<int?> CoveringMarquee,
     [property: JsonPropertyName("fading")] IReadOnlyList<int?> Fading,
     [property: JsonPropertyName("fixed")] IReadOnlyList<int?> Fixed,
-    [property: JsonPropertyName("loading")]
-    IReadOnlyList<int?> Loading,
-    [property: JsonPropertyName("marquee-3")]
-    IReadOnlyList<int?> Marquee3,
-    [property: JsonPropertyName("marquee-4")]
-    IReadOnlyList<int?> Marquee4,
-    [property: JsonPropertyName("marquee-5")]
-    IReadOnlyList<int?> Marquee5,
-    [property: JsonPropertyName("marquee-6")]
-    IReadOnlyList<int?> Marquee6,
-    [property: JsonPropertyName("moving-alternating-3")]
-    IReadOnlyList<int?> MovingAlternating3,
-    [property: JsonPropertyName("moving-alternating-4")]
-    IReadOnlyList<int?> MovingAlternating4,
-    [property: JsonPropertyName("moving-alternating-5")]
-    IReadOnlyList<int?> MovingAlternating5,
-    [property: JsonPropertyName("moving-alternating-6")]
-    IReadOnlyList<int?> MovingAlternating6,
+    [property: JsonPropertyName("loading")] IReadOnlyList<int?> Loading,
+    [property: JsonPropertyName("marquee-3")] IReadOnlyList<int?> Marquee3,
+    [property: JsonPropertyName("marquee-4")] IReadOnlyList<int?> Marquee4,
+    [property: JsonPropertyName("marquee-5")] IReadOnlyList<int?> Marquee5,
+    [property: JsonPropertyName("marquee-6")] IReadOnlyList<int?> Marquee6,
+    [property: JsonPropertyName("moving-alternating-3")] IReadOnlyList<int?> MovingAlternating3,
+    [property: JsonPropertyName("moving-alternating-4")] IReadOnlyList<int?> MovingAlternating4,
+    [property: JsonPropertyName("moving-alternating-5")] IReadOnlyList<int?> MovingAlternating5,
+    [property: JsonPropertyName("moving-alternating-6")] IReadOnlyList<int?> MovingAlternating6,
     [property: JsonPropertyName("off")] IReadOnlyList<int?> Off,
     [property: JsonPropertyName("pulse")] IReadOnlyList<int?> Pulse,
-    [property: JsonPropertyName("rainbow-flow")]
-    IReadOnlyList<int?> RainbowFlow,
-    [property: JsonPropertyName("rainbow-pulse")]
-    IReadOnlyList<int?> RainbowPulse,
-    [property: JsonPropertyName("spectrum-wave")]
-    IReadOnlyList<int?> SpectrumWave,
-    [property: JsonPropertyName("starry-night")]
-    IReadOnlyList<int?> StarryNight,
-    [property: JsonPropertyName("super-breathing")]
-    IReadOnlyList<int?> SuperBreathing,
-    [property: JsonPropertyName("super-fixed")]
-    IReadOnlyList<int?> SuperFixed,
-    [property: JsonPropertyName("super-rainbow")]
-    IReadOnlyList<int?> SuperRainbow,
-    [property: JsonPropertyName("tai-chi")]
-    IReadOnlyList<int?> TaiChi,
-    [property: JsonPropertyName("water-cooler")]
-    IReadOnlyList<int?> WaterCooler,
+    [property: JsonPropertyName("rainbow-flow")] IReadOnlyList<int?> RainbowFlow,
+    [property: JsonPropertyName("rainbow-pulse")] IReadOnlyList<int?> RainbowPulse,
+    [property: JsonPropertyName("spectrum-wave")] IReadOnlyList<int?> SpectrumWave,
+    [property: JsonPropertyName("starry-night")] IReadOnlyList<int?> StarryNight,
+    [property: JsonPropertyName("super-breathing")] IReadOnlyList<int?> SuperBreathing,
+    [property: JsonPropertyName("super-fixed")] IReadOnlyList<int?> SuperFixed,
+    [property: JsonPropertyName("super-rainbow")] IReadOnlyList<int?> SuperRainbow,
+    [property: JsonPropertyName("tai-chi")] IReadOnlyList<int?> TaiChi,
+    [property: JsonPropertyName("water-cooler")] IReadOnlyList<int?> WaterCooler,
     [property: JsonPropertyName("wings")] IReadOnlyList<int?> Wings
 );
 
 public record KrakenDevice(
-    [property: JsonPropertyName("address")]
-    string Address,
-    [property: JsonPropertyName("animation_speeds")]
-    IReadOnlyList<string> AnimationSpeeds,
+    [property: JsonPropertyName("address")] string Address,
+    [property: JsonPropertyName("animation_speeds")] IReadOnlyList<string> AnimationSpeeds,
     [property: JsonPropertyName("bus")] string Bus,
-    [property: JsonPropertyName("color_channels")]
-    ColorChannels ColorChannels,
-    [property: JsonPropertyName("color_modes")]
-    ColorModes ColorModes,
-    [property: JsonPropertyName("description")]
-    string Description,
+    [property: JsonPropertyName("color_channels")] ColorChannels ColorChannels,
+    [property: JsonPropertyName("color_modes")] ColorModes ColorModes,
+    [property: JsonPropertyName("description")] string Description,
     [property: JsonPropertyName("id")] int? Id,
     [property: JsonPropertyName("port")] int? Port,
-    [property: JsonPropertyName("product_id")]
-    int? ProductId,
-    [property: JsonPropertyName("release_number")]
-    int? ReleaseNumber,
-    [property: JsonPropertyName("serial_number")]
-    string SerialNumber,
-    [property: JsonPropertyName("speed_channels")]
-    SpeedChannels SpeedChannels,
-    [property: JsonPropertyName("vendor_id")]
-    int? VendorId
+    [property: JsonPropertyName("product_id")] int? ProductId,
+    [property: JsonPropertyName("release_number")] int? ReleaseNumber,
+    [property: JsonPropertyName("serial_number")] string SerialNumber,
+    [property: JsonPropertyName("speed_channels")] SpeedChannels SpeedChannels,
+    [property: JsonPropertyName("vendor_id")] int? VendorId
 );
 
-public record SpeedChannels(
-    [property: JsonPropertyName("pump")] IReadOnlyList<int?> Pump
-);
+public record SpeedChannels([property: JsonPropertyName("pump")] IReadOnlyList<int?> Pump);
