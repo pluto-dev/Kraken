7284a408eb9a8b515affa14a93fc3b77e31cde1e
pluto <pluto-dev@mailbox.org>
2024-08-13 19:27:23 +0200

    Create SliderAnimationSpeedsConverter

---
+++ src/Kraken.Desktop/Converters/SliderAnimationSpeedsConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace Kraken.Desktop.Converters;
+
+public class SliderAnimationSpeedsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        value switch
+        {
+            0.0 => "Slowest",
+            1.0 => "Slower",
+            2.0 => "Normal",
+            3.0 => "Faster",
+            4.0 => "Fastest",
+            _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+        };
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
