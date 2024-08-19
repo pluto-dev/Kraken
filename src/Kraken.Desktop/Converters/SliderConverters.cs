d5e3e85cb03d417093f6a8e7c98fe4523c29c5ed
pluto <pluto-dev@mailbox.org>
2024-08-19 18:55:14 +0200

    Refactor Slider Converts

---
+++ src/Kraken.Desktop/Converters/SliderConverters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+// ReSharper disable HeapView.BoxingAllocation
+
+namespace Kraken.Desktop.Converters;
+
+public class AnimationSpeedsToValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        value switch
+        {
+            "Slowest" => 0.0,
+            "Slower" => 1.0,
+            "Normal" => 2.0,
+            "Faster" => 3.0,
+            "Fastest" => 4.0,
+            _ => 2.0
+        };
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class ValueToAnimationSpeedsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        value switch
+        {
+            0.0 => "Slowest",
+            1.0 => "Slower",
+            2.0 => "Normal",
+            3.0 => "Faster",
+            4.0 => "Fastest",
+            _ => "Normal"
+        };
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
