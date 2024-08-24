0e181b14d2df16844c6d86316e90a0d00753ed12
pluto <pluto-dev@mailbox.org>
2024-08-24 02:22:46 +0200

    Create methods to convert an array containing ARGB to a base64 string and back

---
+++ src/Kraken.Desktop/Utils/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Kraken.Desktop.Utils;
+
+public class Util
+{
+    public static string BaseifyColorsArray(byte[][] array)
+    {
+        var flattenedArray = array.SelectMany(b => b).ToArray();
+        return Convert.ToBase64String(flattenedArray);
+    }
+
+    public static byte[][] UnBaseifyColorsArray(string base64String)
+    {
+        var flattenedArray = Convert.FromBase64String(base64String);
+        var baseLength = flattenedArray.Length / 4;
+        return Enumerable
+            .Range(0, baseLength)
+            .Select(i => flattenedArray.Skip(i * 4).Take(4).ToArray())
+            .ToArray();
+    }
+}
