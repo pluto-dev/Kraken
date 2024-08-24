f112ab1e2228347ba2a2c39369d8cdc07fe1d844
pluto <pluto-dev@mailbox.org>
2024-08-24 02:26:16 +0200

    Create KrakenDefaultSettings
    Containts the default settings for each color mode

---
+++ src/Kraken.Desktop/Utils/KrakenDefaultSettings.cs
@@ -0,0 +1,741 @@
+﻿namespace Kraken.Desktop.Utils;
+
+public static class KrakenDefaultSettings
+{
+    public const string ColorDefaultSettings = """
+{
+    "ring": {
+        "off": {
+            "mode": "off",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "fixed": {
+            "mode": "fixed",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "fading": {
+            "mode": "fading",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "super-fixed": {
+            "mode": "super-fixed",
+            "minColors": 1,
+            "maxColors": 40,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "spectrum-wave": {
+            "mode": "spectrum-wave",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "marquee-3": {
+            "mode": "marquee-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-4": {
+            "mode": "marquee-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-5": {
+            "mode": "marquee-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-6": {
+            "mode": "marquee-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "covering-marquee": {
+            "mode": "covering-marquee",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-3": {
+            "mode": "alternating-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-4": {
+            "mode": "alternating-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-5": {
+            "mode": "alternating-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-6": {
+            "mode": "alternating-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-3": {
+            "mode": "moving-alternating-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-4": {
+            "mode": "moving-alternating-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-5": {
+            "mode": "moving-alternating-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-6": {
+            "mode": "moving-alternating-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "pulse": {
+            "mode": "pulse",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "breathing": {
+            "mode": "breathing",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "super-breathing": {
+            "mode": "super-breathing",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 40,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "candle": {
+            "mode": "candle",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    255,
+                    165,
+                    0
+                ]
+            ]
+        },
+        "starry-night": {
+            "mode": "starry-night",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "rainbow-flow": {
+            "mode": "rainbow-flow",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "super-rainbow": {
+            "mode": "super-rainbow",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "rainbow-pulse": {
+            "mode": "rainbow-pulse",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "loading": {
+            "mode": "loading",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "tai-chi": {
+            "mode": "tai-chi",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "water-cooler": {
+            "mode": "water-cooler",
+            "animationSpeed": "normal",
+            "minColors": 2,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "wings": {
+            "mode": "wings",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        }
+    },
+    "logo": {
+        "off": {
+            "mode": "off",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "fixed": {
+            "mode": "fixed",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "fading": {
+            "mode": "fading",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "super-fixed": {
+            "mode": "super-fixed",
+            "minColors": 1,
+            "maxColors": 40,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "spectrum-wave": {
+            "mode": "spectrum-wave",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "marquee-3": {
+            "mode": "marquee-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-4": {
+            "mode": "marquee-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-5": {
+            "mode": "marquee-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "marquee-6": {
+            "mode": "marquee-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "covering-marquee": {
+            "mode": "covering-marquee",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-3": {
+            "mode": "alternating-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-4": {
+            "mode": "alternating-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-5": {
+            "mode": "alternating-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "alternating-6": {
+            "mode": "alternating-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-3": {
+            "mode": "moving-alternating-3",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-4": {
+            "mode": "moving-alternating-4",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-5": {
+            "mode": "moving-alternating-5",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "moving-alternating-6": {
+            "mode": "moving-alternating-6",
+            "animationSpeed": "normal",
+            "direction": "forward",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "pulse": {
+            "mode": "pulse",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "breathing": {
+            "mode": "breathing",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 8,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "super-breathing": {
+            "mode": "super-breathing",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 40,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "candle": {
+            "mode": "candle",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    255,
+                    165,
+                    0
+                ]
+            ]
+        },
+        "starry-night": {
+            "mode": "starry-night",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "rainbow-flow": {
+            "mode": "rainbow-flow",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "super-rainbow": {
+            "mode": "super-rainbow",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "rainbow-pulse": {
+            "mode": "rainbow-pulse",
+            "animationSpeed": "normal",
+            "minColors": 0,
+            "maxColors": 0
+        },
+        "loading": {
+            "mode": "loading",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "tai-chi": {
+            "mode": "tai-chi",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "water-cooler": {
+            "mode": "water-cooler",
+            "animationSpeed": "normal",
+            "minColors": 2,
+            "maxColors": 2,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        },
+        "wings": {
+            "mode": "wings",
+            "animationSpeed": "normal",
+            "minColors": 1,
+            "maxColors": 1,
+            "colors": [
+                [
+                    77,
+                    0,
+                    116
+                ]
+            ]
+        }
+    }
+}
+""";
+}
