d61d98e05cb10c818c935276ba01bb325ca5c119
pluto <pluto-dev@mailbox.org>
2024-08-24 12:53:23 +0200

    Refactor LightingPage view

--- src/Kraken.Desktop/Views/LightingPage.xaml.cs
+++ src/Kraken.Desktop/Views/LightingPage.xaml.cs
@@ -1,18 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Kraken.Desktop.Controls;
+using Kraken.Desktop.Services;
+using Kraken.Desktop.Utils;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Kraken.Desktop.Views;
 
-namespace Kraken.Desktop.Views
+public sealed partial class LightingPage : Page, INotifyPropertyChanged
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class LightingPage : Page
+    public LightingPage()
     {
-        public LightingPage()
+        InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private readonly KrakenService _krakenService = KrakenService.Instance;
+    private readonly StorageService _storageService = StorageService.Instance;
+
+    private KrakenDevice? _krakenDevice;
+    private string _krakenDeviceName = string.Empty;
+
+    public string KrakenDeviceName
+    {
+        get => _krakenDeviceName;
+        set
         {
-            this.InitializeComponent();
+            _krakenDeviceName = value;
+            OnPropertyChanged();
         }
     }
+
+    private async void LightingPage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var devices = await _krakenService.GetDevices();
+
+        _krakenDevice = devices?.FirstOrDefault(x => x is { ProductId: 8199, VendorId: 7793 });
+
+        if (_krakenDevice is null)
+            return;
+
+        KrakenDeviceName = _krakenDevice.Description;
+
+        IList<Tuple<string, IReadOnlyList<int?>>> deviceColorModes = [];
+
+        foreach (var (key, value) in _krakenDevice.ColorModes)
+        {
+            deviceColorModes.Add(new Tuple<string, IReadOnlyList<int?>>(key, value));
+        }
+
+        var colorModesSettings = await _storageService.ReadColorModeSettingsAsync();
+        if (colorModesSettings is null)
+        {
+            throw new InvalidOperationException("Couldn't retrieve color modes settings");
+        }
+        var textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+        foreach (var key in _krakenDevice.ColorChannels.Keys.Where(key => key is "ring" or "logo"))
+        {
+            LedStackPanel.Children.Add(
+                new LedControl(
+                    _krakenDevice,
+                    deviceColorModes,
+                    colorModesSettings,
+                    textInfo.ToTitleCase(key)
+                )
+            );
+        }
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
